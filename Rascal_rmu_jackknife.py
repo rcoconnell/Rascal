b2f1d2e45744d17834a08a01fdb3434cabde5db3
Ross O'Connell <rcoconne@Rosss-MBP.wv.cc.cmu.edu>
2017-03-15 16:09:06 -0400

    Quick and dirty addition of the jackknife

--- Rascal_rmu_jackknife.py
+++ Rascal_rmu_jackknife.py
@@ -1,12 +1,14 @@
 """
 This version of Rascal generates a covariance matrix for a 2-point correlation function
 evaluated in bins of (r,mu). It implements the method described in O'Connell et al, arxiv:1510.01740
+
+It has been modified to compute a model version of a jackknife covariance.
 """
 
 from ConfigParser import SafeConfigParser
 from multiprocessing import Pool
 from Rascal_Sampler import *
-from numpy import loadtxt,savetxt,max,min,interp,abs,linspace,meshgrid,zeros,ones,cumsum,where,reshape,shape,array,mean,arange,std,trace,log,diag,outer
+from numpy import loadtxt,savetxt,max,min,interp,abs,linspace,meshgrid,zeros,ones,cumsum,where,reshape,shape,array,mean,arange,std,trace,log,diag,outer,arccos
 from numpy.linalg import det,inv,eigvals
 from scipy.interpolate import RectBivariateSpline,interp1d
 from scipy.optimize import fmin
@@ -15,6 +17,7 @@ import pickle
 from numpy.random import seed
 import Rascal_Classes as Rascal_classes
 import sys
+import healpy as hp
 
 if len(sys.argv)!=2:
 	print "Config file?"
@@ -48,7 +51,7 @@ print "Mask is bounded in dec by "+str(mysurvey.decmin)+"-"+str(mysurvey.decmax)
 #######
 
 #Some flags
-RASCAL_version = '0.95'
+RASCAL_version = '0.96'
 
 #More sampler options
 ncores  = Config.getint('Sampler','ncores')
@@ -66,6 +69,24 @@ dr  = rmins[1]  - rmins[0]
 for ri in range(len(rmins)):
 	for mui in range(len(mumins)):
 		bnl.append( bin(rmins[ri],mumins[mui],dr,dmu) )
+
+######
+
+def quick_px(r_vec):
+	#Copied from BOSS_Like_Survey
+	r = anorm(r_vec)
+	x,y,z = transpose(r_vec)
+	dec = arccos(x/r)*(180/pi) - 90
+	ra  = arctan2(z,y) * (180/pi)
+	f = where(ra<0)
+	ra[f] += 360
+	
+	#Copied from splitter
+	theta = (dec+90)*pi/180
+	phi = ra*pi/180
+	px = hp.ang2pix(8,theta,phi)
+
+	return px
 
 ######
 
@@ -90,12 +111,19 @@ def dosamples(bn):
 	rij,p = flat_draw(NSamp,bn)
 	r4n2 , dp , ri_vec , rj_vec ,wi,wj= mysurvey.maskpair(rij)
 	p *= dp
+	ri_px = quick_px(ri_vec)
+	rj_px = quick_px(rj_vec)
+	rij_jack = (ri_px==rj_px).astype(int)
 
 	rij_mag,rij_mu = cleanup_l(ri_vec-rj_vec,0.5*(ri_vec+rj_vec))
 
-	Ra  = mean( r4n2 / p )
-	ct2 = mean( r4n2 * wi*wj * (1+mysurvey.xi(rij_mag,rij_mu)) / p )
-	ctx = mean( r4n2 * mysurvey.xi(rij_mag,rij_mu) / p )
+#	Ra  = mean( r4n2 / p )
+#	ct2 = mean( r4n2 * wi*wj * (1+mysurvey.xi(rij_mag,rij_mu)) / p )
+#	ctx = mean( r4n2 * mysurvey.xi(rij_mag,rij_mu) / p )
+
+	Ra  = mean( rij_jack * r4n2 / p )
+	ct2 = mean( rij_jack * r4n2 * wi*wj * (1+mysurvey.xi(rij_mag,rij_mu)) / p )
+	ctx = mean( rij_jack * r4n2 * mysurvey.xi(rij_mag,rij_mu) / p )
 
 	for rpt in range(repeats):
 		rjk,p_jk = r2xi_draw(NSamp,mysurvey.r2xi)
@@ -105,6 +133,11 @@ def dosamples(bn):
 		rl_vec = ri_vec - ril
 		rk = anorm(rk_vec)
 		rl = anorm(rl_vec)
+		rk_px = quick_px(rk_vec)
+		rl_px = quick_px(rl_vec)
+		rkl_jack = (rk_px==rl_px).astype(int)
+		rik_jack = (ri_px==rk_px).astype(int)
+		rjl_jack = (rj_px==rl_px).astype(int)
 
 		rkl_mag,rkl_mu = cleanup_l(rk_vec-rl_vec,0.5*(rk_vec+rl_vec))
 		rjk_mag,rjk_mu = cleanup_l(rj_vec-rk_vec,0.5*(rj_vec+rk_vec))
@@ -121,6 +154,7 @@ def dosamples(bn):
 		xijk[m] = mysurvey.xi(rjk_mag[m],rjk_mu[m])
 		xiil[m] = mysurvey.xi(ril_mag[m],ril_mu[m])
 		
+		
 		nwk = zeros(len(rk))
 		nwl = zeros(len(rl))
 		nwall = mysurvey.nw( concatenate((rk_vec[m],rl_vec[m])) )
@@ -135,19 +169,22 @@ def dosamples(bn):
 				f = where( (rkl_mag>rmins[rj]) & (rkl_mag<rmins[rj]+4) & 
 						   (abs(rkl_mu)>mumins[muj]) & (abs(rkl_mu)<mumins[muj]+0.1) )
 
-				ct4[j] += sum( ((r4n2 * rjk_mag**2 * xijk * nwk * ril_mag**2 * xiil * nwl)/(p*p_jk*p_il))[f] )/NSamp
+				#ct4[j] += sum( ((r4n2 * rjk_mag**2 * xijk * nwk * ril_mag**2 * xiil * nwl)/(p*p_jk*p_il))[f] )/NSamp
+				ct4[j] += sum( ((rij_jack * rkl_jack * r4n2 * rjk_mag**2 * xijk * nwk * ril_mag**2 * xiil * nwl)/(p*p_jk*p_il))[f] )/NSamp
 
 				#First 3-point term
 				f = where( (rik_mag>rmins[rj]) & (rik_mag<rmins[rj]+4) & 
 						   (abs(rik_mu)>mumins[muj]) & (abs(rik_mu)<mumins[muj]+0.1) )
 
-				ct3[j] += sum( ((r4n2 * rjk_mag**2 * xijk * nwk * wi)/(p*p_jk))[f] )/NSamp
+				#ct3[j] += sum( ((r4n2 * rjk_mag**2 * xijk * nwk * wi)/(p*p_jk))[f] )/NSamp
+				ct3[j] += sum( ((rij_jack * rik_jack * r4n2 * rjk_mag**2 * xijk * nwk * wi)/(p*p_jk))[f] )/NSamp
 
 				#Second 3-point term
 				f = where( (rjl_mag>rmins[rj]) & (rjl_mag<rmins[rj]+4) & 
 						   (abs(rjl_mu)>mumins[muj]) & (abs(rjl_mu)<mumins[muj]+0.1) )
 
-				ct3[j] += sum( ((r4n2 * ril_mag**2 * xiil * nwl * wj)/(p*p_il))[f] )/NSamp
+				#ct3[j] += sum( ((r4n2 * ril_mag**2 * xiil * nwl * wj)/(p*p_il))[f] )/NSamp
+				ct3[j] += sum( ((rij_jack * rjl_jack * r4n2 * ril_mag**2 * xiil * nwl * wj)/(p*p_il))[f] )/NSamp
 				
 	ct4 /= repeats
 	ct3 /= repeats
